b4b4714222d8a14c4ad6cec2c03bc42e3a6b12a3
Joshua Smith <smith.joshuasterling@gmail.com>
2020-04-22 23:24:52 -0600

    create get by ID endpoint and refactor

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,33 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
-/* GET home page. */
-router.get('/search/:name', function (req, res, next) {
+/* GET Superheros by Name. */
+router.get("/search/:name", function (req, res, next) {
+  console.log(req.params.name);
   axios
     .get(
       `https://superheroapi.com/api/${process.env.ACCESS_TOKEN}/search/${req.params.name}`
     )
-    .then((result) => {
-      res.status(200).json(result.data.results);
+    .then(({ data }) => {
+      res.status(200).json(data.results);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+/* GET Superhero by ID */
+router.get("/superhero/:id", function (req, res, next) {
+  axios
+    .get(
+      `https://superheroapi.com/api/${process.env.ACCESS_TOKEN}/${req.params.id}`
+    )
+    .then(({ data }) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
     });
 });
 
